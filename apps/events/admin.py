833406579f5f987a93fbec2213d863629d3be95e
kiarash seyedmohamadi <seyedmohamadikiarash@gmail.com>
2025-08-21 12:41:31 +0330

    Configure MySQL database and register models in admin

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event, Booking, Payment, Venue
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(Venue)
+
+
