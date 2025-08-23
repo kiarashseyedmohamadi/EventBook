5bdf164075777db9cdfbba5cae2616efd5215a89
kiarash seyedmohamadi <seyedmohamadikiarash@gmail.com>
2025-08-23 18:09:46 +0330

    Add URLs for Venue, Event, Booking, and Payment views

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -6,4 +6,20 @@ app_name='events'
 
 urlpatterns = [
      path('',views.TestAPIView.as_view(), name='register'),
+     
+     # Venue
+     path('venues/', views.VenueView.as_view(), name='venue-list'),  
+     path('venues/<int:pk>/', views.VenueView.as_view(), name='venue-detail'), # PUT, PATCH, DELETE
+
+     # Event
+     path('events/', views.EventView.as_view(), name='event-list'),           # GET و POST
+     path('events/<int:pk>/', views.EventView.as_view(), name='event-detail'), # PUT, PATCH, DELETE
+
+     # Booking
+     path('bookings/', views.BookingView.as_view(), name='booking-list'),        # GET و POST
+     path('bookings/<int:pk>/', views.BookingView.as_view(), name='booking-detail'), # DELETE
+
+     # Payment
+     path('payments/<int:pk>/', views.PaymentView.as_view(), name='payment'),        # GET و POST
+          
 ]
